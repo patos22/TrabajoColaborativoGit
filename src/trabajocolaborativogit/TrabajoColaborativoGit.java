6b1d2ec906761635795125fefbeb554e4664ca45
patos22 <kv7297763@gmail.com>
2022-08-08 14:01:36 -0600

    Creacion del proyectoColaborativoGit

---
+++ src/trabajocolaborativogit/TrabajoColaborativoGit.java
@@ -0,0 +1,15 @@
+
+package trabajocolaborativogit;
+
+/**
+ *
+ * @author Aprendiente
+ */
+public class TrabajoColaborativoGit {
+
+   
+    public static void main(String[] args) {
+      
+    }
+    
+}
